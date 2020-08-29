08a70e2b0833e2c753739527f940cf0c377c060f
Dan Vanghelicescu <dan.vanghelicescu@fifa.org>
2020-08-29 15:50:08 +0300

    untracked files on feature-01-dav: 9f4f7d3 27.08

---
+++ snake/Food.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Snake
+{
+    public class Food
+    {
+        public int Value { get; private set; }
+        public Position Position { get; private set; }
+
+        public Food()
+        {
+            Value = 0;
+        }
+
+        public void SetPositionAndValue(Position position)
+        {
+            Position = position;
+            SetNextFoodValue();
+        }
+
+        private void SetNextFoodValue()
+        {
+            Value++;
+        }
+    }
+}
---
+++ snake/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Snake
+{
+    public class Position : IComparable
+    {
+        public int column;
+        public int row;
+
+        public Position(int sendColumn, int sendRow) => (column, row) = (sendColumn, sendRow);
+
+        public bool IsOutOfGame(int columns, int rows) => row < 0
+                                                        || row >= rows
+                                                        || column < 0
+                                                        || column >= columns;
+
+        #region Implementing IComparable
+        public int CompareTo(object obj) => 0;
+
+        public bool Equals(Position other) => column == other.column && row == other.row;
+        #endregion
+    }
+}
